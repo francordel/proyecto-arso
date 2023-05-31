b5c8d86a89dd36c3460915497acc94b14f07e6eb
Fran Cortés <100516685+francordel@users.noreply.github.com>
2023-05-31 12:55:39 +0200

    funcionando arso

--- pasarela/src/main/java/pasarela/zuul/seguridad/SecurityConfig.java
+++ pasarela/src/main/java/pasarela/zuul/seguridad/SecurityConfig.java
@@ -1,15 +1,12 @@
 package pasarela.zuul.seguridad;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
